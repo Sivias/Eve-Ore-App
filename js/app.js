498718083f33e51c4363bcce9cffd10e2e1a3157
Jordan Prosser <Jordan Prosser>
2017-02-04 13:56:41 -0600

    rearange ore

--- js/app.js
+++ js/app.js
@@ -16,6 +16,17 @@ $(document).ready(function(){
     $('#goldenomb').text('Golden Omber' + ' - ');
     $('#silveromb').text('Silver Omber' + ' - ');
 
+        // Ore types
+    var oreNames = [
+        'pyroxeres', 'viscousPyro', 'solidPyro',
+        'kernite', 'fieryKern', 'luminousKern',
+        'veldspar', 'denseVeld', 'concentratedVeld',
+        'scordite', 'massiveScord', 'condensedScord',
+        'omber', 'goldenOmb', 'silverOmb',
+        'radiantHermo', 'vividHermo',
+        'hedbergite'
+    ];
+
     var ores = {}
 
         // Update ore value based on user submission
@@ -33,6 +44,39 @@ $(document).ready(function(){
             // Pretty sure this can be compacted (Copy pasta is the devil)  Also auto-ordered by venture hold value.
         
         $('#'+ore.oreName).text(ore.displayName + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+
+        // if(oreName.indexOf('kernite') >= 0){
+        //     $('#ore-1').text('Kernite' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('fierykern') >= 0){
+        //     $('#ore-2').text('Fiery Kernite' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('luminouskern') >= 0){
+        //     $('#ore-3').text('Luminous Kernite' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('pyroxeres') >= 0){
+        //     $('#ore-4').text('Pyroxeres' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('viscouspyro') >= 0){
+        //     $('#ore-5').text('Viscous Pyroxeres' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('solidpyro') >= 0){
+        //     $('#ore-6').text('Solid Pyroxeres' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('veldspar') >= 0){
+        //     $('#ore-7').text('Veldspar' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('denseveld') >= 0){
+        //     $('#ore-8').text('Dense Veldspar' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('concentratedveld') >= 0){
+        //     $('#ore-9').text('Concentrated Veldspar' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('scordite') >= 0){
+        //     $('#ore-10').text('Scordite' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('massivescord') >= 0){
+        //     $('#ore-11').text('Massive Scordite' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('condensedscord') >= 0){
+        //     $('#ore-12').text('Condensed Scordite' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('omber') >= 0){
+        //     $('#ore-13').text('Omber' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('goldenomb') >= 0){
+        //     $('#ore-14').text('Golden Omber' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // } else if(oreName.indexOf('silveromb') >= 0){
+        //     $('#ore-15').text('Silver Omber' + ' - ' + oreVal + ' | Venture Value: ' + oreHoldValue(ore.m3, oreVal, venture.oreHold));
+        // }
+        
         refreshOreList();
     });
 
@@ -48,22 +92,31 @@ $(document).ready(function(){
         this.displayName = displayName
     };
 
+        // Ships
+
+    function Ship(oreHold, miningRate) {
+        this.oreHold = oreHold;
+        this.miningRate = miningRate;
+    };
+
+    var venture = new Ship(5000, null);
+    var covetor = new Ship(7000, null);
+    var retriever = new Ship(23100, null);
+
+            // Calculate max hold value per ship type
+    function oreHoldValue(oreM3, estIsk, shipOreHold) {
+        if(estIsk) {
+            return (((1 / oreM3) * estIsk) * shipOreHold);
+        }
+    };
+
+    
         // Security
     var nullSec = 'Null Security';
     var lowSec = 'Low Security';
     var highSec = 'High Security';
     var anomoly = 'Anomoly';
 
-        // Ore types
-    var oreNames = [
-        'pyroxeres', 'viscousPyro', 'solidPyro',
-        'kernite', 'fieryKern', 'luminousKern',
-        'veldspar', 'denseVeld', 'concentratedVeld',
-        'scordite', 'massiveScord', 'condensedScord',
-        'omber', 'goldenOmb', 'silverOmb',
-        'radiantHermo', 'vividHermo',
-        'hedbergite'
-    ]
 
         // Ore loop to assign Ore values to the Ore Types array that
         // can be referenced on the HTML page (data-name="")
