17cc7eb189b2f02b15771e65a4e8b6ff17b31d7e
Jordan Prosser <Jordan Prosser>
2017-02-11 22:59:21 -0600

    Raw Text box is creating objects for JSON!

--- js/rawOreText.js
+++ js/rawOreText.js
@@ -1,22 +1,31 @@
 $(document).ready(function(){
-    var ore = []
 
     var myOre = $('.raw_textInput').on('submit', function(e) {
         e.preventDefault();
-        var ores = {};
+            
+            // Split the paste at carriage returns
         var oreAtCR = $('#raw_textArea').val().split('\n');
+        var ore = { data:[] };
 
+            // Loop through all carriage return user pasted values
         for (i = 0; i < oreAtCR.length; i++){
             var oreAtTab = oreAtCR[i];
+                // Split the Carriage Return paste at 'tab'
             var minerals = oreAtTab.split('\t').filter(function(str){
                 return str !== '';
             });
-            ore.push(minerals);
+                // Create the Object
+            var tempOre = {
+                oreName: minerals[0],
+                oreQty: minerals[1],
+                oreType: minerals[2],
+                oreVolume: minerals[3],
+            };
+                // Push the object into 'ore' array, ready for JSON
+            ore.data.push(tempOre);
         };
-        
-        for (i=0; i<ore.length; i++){
-            console.log(ore[i]);
-        }
 
+            // Output!!!
+        console.log(ore);
     });
 });
